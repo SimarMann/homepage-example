d8e87cab0d627d4a95ff14a956cbbb6960bb0063
SimarMann <47508510+SimarMann@users.noreply.github.com>
2023-05-18 01:13:50 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,16 +16,17 @@ function changeIcon() {
     iconState.classList.remove('open-icon');
     iconState.classList.add('close-icon');
     let close = document.querySelector('.close-icon');
-    if (string === '/tech.html' || string === '/neuropsy.html' || string === '/econ.html' || string === '/intrel.html') {
+    if (string === '/homepage-example/tech.html' || string === '/homepage-example/neuropsy.html' || string === '/homepage-example/econ.html' || string === '/homepage-example/intrel.html') 
+    {
       close.style.backgroundImage = 'url(../img/tech/close.svg)';
     }
   } else if (iconState.classList.contains('close-icon')) {
     iconState.classList.remove('close-icon');
     iconState.classList.add('open-icon');
     let open = document.querySelector('.open-icon');
-    if (string === '/tech.html' || string === '/neuropsy.html') {
+    if (string === '/homepage-example/tech.html' || string === '/homepage-example/neuropsy.html') {
       open.style.backgroundImage = 'url(../img/tech/menu.svg)';
-    } else if (string === '/econ.html' || string === '/intrel.html') {
+    } else if (string === '/homepage-example/econ.html' || string === '/homepage-example/intrel.html') {
       open.style.backgroundImage = 'url(../img/econ/menu.svg)';
     }
   }
